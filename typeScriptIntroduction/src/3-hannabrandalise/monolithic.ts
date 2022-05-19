71aff8b201042f422570b9ef57f74ffa2a2b4be3
albertovictorrebello <albertorebello@gmail.com>
2022-05-19 07:54:06 -0300

    Typescript tests

--- typeScriptIntroduction/src/3-hannabrandalise/monolithic.ts
+++ typeScriptIntroduction/src/3-hannabrandalise/monolithic.ts
@@ -7,8 +7,8 @@ interface IPerson{
     gender: string;
 }
 //From function.ts
-function getPersonType(info: IPerson[] , type: string){
-    const findPerson = info.find ( item => item.gender === type )
+function getPersonType(info: IPerson[] , type: string) { 
+    const findPerson = info.find(item => item.gender === type)       
     return findPerson
 }
 
@@ -30,12 +30,18 @@ const PersonB: IPerson = {
     gender: 'Female'
 };
 
-
 //array contendo dois objetos e interface compondo array
 const AllPersons: IPerson[] = [PersonA, PersonB];
 
-
 // From index.ts
-console.log (AllPersons);
-console.log ('Retorna genero feminino', getPersonType);
-console.log ('Retona generomasculino', getPersonType)
+console.log("AllPersons");
+/* AR: nested into JSON.stringify() */
+console.log (JSON.stringify(AllPersons));
+
+/* AR: Changed from getPersonType to getPersonType(AllPersons, "Female")) 
+       and nested into JSON.stringify()*/
+console.log ('Retorna genero feminino', JSON.stringify(getPersonType(AllPersons, "Female")));
+
+/* AR: Changed from getPersonType to getPersonType(AllPersons, "Male")) 
+       and nested into JSON.stringify()*/
+console.log ('Retona generomasculino', JSON.stringify(getPersonType(AllPersons, "Male")))
