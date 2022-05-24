ff11a09f2d90cb42f8ac641bd633b1aa0319cfa0
albertovictorrebello <albertorebello@gmail.com>
2022-05-23 22:50:46 -0300

    finished lesson 8

--- clientServerExampleWithGraphQL/packages/server/src/main.js
+++ clientServerExampleWithGraphQL/packages/server/src/main.js
@@ -1,6 +1,4 @@
 import { createServer } from 'http';
-import { readFile } from 'fs';
-import { resolve } from 'path';
 import { parse } from 'querystring'
 const server = createServer((request, response) => {
     switch (request.url) {
@@ -15,48 +13,15 @@ const server = createServer((request, response) => {
             );
             response.end();
             break;
-        }    
-
-        case '/sign-in': {
-            const path = resolve(__dirname, '../../web/sign-in.html');
-            
-            readFile(path, (error, file) => {
-                if (error) {
-                    response.writeHead('500', 'Can\'t process HTML file.');
-                    response.end();
-                    return;
-                }
-                response.writeHead('200');
-                response.write(file);
-                response.end();                              
-            });
-            break;
-        }   
-        
-        case '/home': {
-            const path = resolve(__dirname, '../../web/home.html');
-            readFile(path, (error, file) => {
-                if (error) {
-                    response.writeHead('500', 'Can\'t process HTML file.');
-                    response.end();
-                    return;
-                }
-                response.writeHead('200');
-                response.write(file);
-                response.end();                              
-            });
-            break;
-            
-        }
+        }          
+       
         case '/authenticate': {
             let data = '';
             request.on('data', (chunk) => {
                 data += chunk;
             });
-            request.on('end', () => {                
-                response.writeHead(301, {
-                    Location: '/home'
-                });
+            request.on('end', () => {  
+                const params = parse(data);                             
                 response.end();
             });
             break;
