9c8f4c60180e3aa548cfff6a09c4a9a30a692d1e
albertovictorrebello <albertorebello@gmail.com>
2022-05-24 05:32:44 -0300

    finished lesson 14

--- clientServerExampleWithGraphQL/packages/server/src/main.js
+++ clientServerExampleWithGraphQL/packages/server/src/main.js
@@ -1,12 +1,51 @@
 import express from 'express';
-import cors from   'cors';
+import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
-const server = express();
+const app = express();
+async function startServer() {
+    const server = new ApolloServer({
+        plugins: [
+            ApolloServerPluginLandingPageGraphQLPlayground(),
+        ],
+        typeDefs: gql`
+        type Client {
+            id: ID!
+            name: String!
+        }
+        
+        type Demand {
+                id: ID!
+                name: String!
+                client: Client!
+                deadline: String
+            }
+
+            type Query {
+                demands: [Demand]!
+            }
+
+        `,
+        resolvers: {
+            Query: {
+                demands: () => []
+            }
+        }
+    });
+    await server.start();
+    server.applyMiddleware({
+        app,
+        cors: { origin: 'http://localhost:3000'},
+        bodyParserConfig: true
+    });
+};
+startServer(app);
 
 /*This implements cors to all requisitions 
-server.use(cors); */
+app.use(cors); */
 
-server.get('/status',(_, response) => {
+/* app.get('/status',(_, response) => {
     response.send(
         {
             status:'Keep walking...express'
@@ -16,7 +55,7 @@ server.get('/status',(_, response) => {
 
 const enableCors = cors({ origin: 'http://localhost:3000'});
 
-server
+app
 .options('/authenticate', enableCors)
 .post('/authenticate', enableCors, express.json(), (request, response) => {
     console.log(
@@ -26,11 +65,18 @@ server
     response.send({
         Okay: true
     });
-} );
+} ); */
 
 const portNumber = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const hostName = process.env.HOSTNAME || '127.0.0.1';
 
-server.listen(portNumber, hostName, () => {
-    console.log(`Server listening is listening at http://${ hostName }:${ portNumber }`);
+app.listen(portNumber, hostName, () => {
+    console.log(`app listening is listening at http://${ hostName }:${ portNumber }`);
 });
+
+/* modified:   
+	modified:   
+	modified:   
+	modified:   
+	modified:   
+ */
