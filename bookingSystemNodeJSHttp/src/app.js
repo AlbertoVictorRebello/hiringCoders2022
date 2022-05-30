ef0b88db4ae053ef85b780e60cafc8a853f86e6f
albertovictorrebello <albertorebello@gmail.com>
2022-05-30 15:38:51 -0300

    clean up

---
+++ bookingSystemNodeJSHttp/src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const routes = require('./router')
+
+class App {
+    constructor(){
+        this.server = express();
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares(){
+        this.server.use(express.json());
+    }
+
+    routes(){
+        this.server.use(routes);
+    }
+}
+
+module.exports = new App().server;
---
+++ bookingSystemNodeJSHttp/src/server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+const PORT = 3333;
+
+
+app.listen(PORT), '127.0.0.1';
