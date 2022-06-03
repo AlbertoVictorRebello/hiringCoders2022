d26adf673441bcc81172fba86c24b4e0abd669b2
albertovictorrebello <albertorebello@gmail.com>
2022-06-03 02:35:21 -0300

    implement yup on SessionController

--- bookingSystemNodeJSHttp/src/app/controllers/SessionController.js
+++ bookingSystemNodeJSHttp/src/app/controllers/SessionController.js
@@ -1,10 +1,23 @@
-import User from "../models/User";
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
+import User from "../models/User";
 import authConfig from "../../config/auth";
+
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({            
+            email: Yup.string().email().required("A valid email shall be provided"),
+            password: Yup.string().required()  
+          });
+      
+          if (!(await schema.isValid(req.body))) {
+              return res.status(400).json({
+                  message: 'Schema validation failure'
+              })
+      
+          }
         const { email, password } = req.body;
         const user = await User.findOne({
             where: { email }
