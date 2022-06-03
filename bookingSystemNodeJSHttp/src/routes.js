2e05c5477c2adc29feb14b754a2a2147330a6fbe
albertovictorrebello <albertorebello@gmail.com>
2022-06-03 03:48:05 -0300

    finish lesson 13

--- bookingSystemNodeJSHttp/src/routes.js
+++ bookingSystemNodeJSHttp/src/routes.js
@@ -1,9 +1,12 @@
 import { Router} from 'express'; 
+import multer from 'multer';
+import multerConfig from './config/multer'
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController'
 import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 
 routes.post('/users', UserController.store)
@@ -19,4 +22,8 @@ routes.use(authMiddleware)
 
 routes.put('/users', UserController.update)
 
+//File uploads
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({message: 'Ok'})
+})
 export default routes;
